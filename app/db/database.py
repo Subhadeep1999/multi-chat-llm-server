99dd30360b621fc2fdd4c6a2ca6ece46361d7e1c
subhadeep1999 <subhadeepsahoo258@gmail.com>
2026-02-14 13:51:11 +0530

    comment out load_dotenv() call in database.py

--- app/db/database.py
+++ app/db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-load_dotenv()
+# load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
